4bc03c0733940ed11b85d05af2ef4788e9e498ac
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-11-22 21:06:59 +0300

    Создание маршрутизатора

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  
+/* Страница с капучино. */
+router.get('/kapuchino', function(req, res, next) {
+  res.render('<h1>Капучино<h1>');
+});
+
+router.get('/espresso', function(req, res, next) {
+  res.render('<h1>Эспрессо<h1>');
+});
+
+router.get('/latte', function(req, res, next) {
+  res.render('<h1>Латте<h1>');
+});
+
+router.get('/americano', function(req, res, next) {
+  res.render('<h1>Американо<h1>');
 });
 
 module.exports = router;

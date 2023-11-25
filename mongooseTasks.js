f55b5b186cc1834c611a2d0d4fb3361bc2298dc9
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-11-25 22:05:17 +0300

    Создание схемы

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -5,8 +5,10 @@ var schema = mongoose.Schema({ name: String })
 schema.methods.peculiarity = function(){ 
     console.log(this.get("name") + " с ванильным сиропом")
 }
+
 const Cup = mongoose.model('Cup', schema); 
 const cup = new Cup({ name: 'Раф' })
+
 cup.save().then(() => {
     cup.peculiarity()
 })

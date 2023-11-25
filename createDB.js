6dce651bca33c56fd9d627352191caf0fe760bae
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-11-25 22:07:47 +0300

    Создание схемы

--- createDB.js
+++ createDB.js
@@ -1,8 +1,11 @@
 var MongoClient = require('mongodb').MongoClient 
 var data = require("./data.js").data
+
 const uri = "mongodb://localhost:27017/"
 const client = new MongoClient(uri)
+
 async function run() {
+
     try {
         await client.connect();
         var database = client.db("coffee"); database.dropDatabase()
@@ -11,7 +14,7 @@ async function run() {
         const result = await cup.insertMany(data); 
         console.log(`${result} documents were inserted`);
     } 
-    
+
     finally {
            await client.close();
     } 

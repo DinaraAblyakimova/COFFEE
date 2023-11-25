4ac8a6338bd6317e2b74756d80e8f79514829ed2
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-11-25 15:23:25 +0300

    Подключение базы данных mongodb

---
+++ createDB.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient 
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try {
+        await client.connect();
+        var database = client.db("coffee"); database.dropDatabase()
+        database = client.db("coffee");
+        const cats = database.collection("cup");
+        const result = await cats.insertOne({name:"Капучино"}); 
+        console.log(`${result} documents were inserted`);
+    } 
+    finally {
+           await client.close();
+    } 
+}
+run()

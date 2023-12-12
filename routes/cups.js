b748e0bf70f601cc8aaa61d148a4e951378813d1
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-12-12 20:29:56 +0300

    Заполнение меню

--- routes/cups.js
+++ routes/cups.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Cup = require("../models/cup").Cup;
+var async = require("async")
 
 // /* GET users listing. */
 // router.get('/', (req, res, next) => {
@@ -9,25 +10,58 @@ const Cup = require("../models/cup").Cup;
 
 
 /* Страница героев */
-router.get("/:nick", async (req, res, next) => {
-  try {
-    const cup = await Cup.findOne({ nick: req.params.nick });
-    console.log(cup);
-    if (!cup) {
-      throw new Error("Нет такого!");
+  router.get('/:nick', async function(req, res, next) {
+    try {
+      const [cup, cups] = await Promise.all([
+        Cup.findOne({ nick: req.params.nick }),
+        Cup.find({}, { _id: 0, title: 1, nick: 1 })
+      ]);
+    
+      if (!cup) {
+        throw new Error("Нет такого");
+      }
+      
+      renderCup(res, cup.title, cup.avatar, cup.desc, cups);
+    } catch (err) {
+      next(err);
     }
+  });
+  
+  function renderCup(res, title, picture, desc, cups) {
+    console.log(cups);
+  
     res.render('cup', {
-      title: cup.title,
-      picture: cup.avatar,
-      desc: cup.desc
+      title: title,
+      picture: picture,
+      desc: desc,
+      menu: cups
     });
-  } catch (err) {
-    next(err);
   }
-});
+  
+  
+  
+  module.exports = router;
+  
+
+// try {
+//     const cup = await Cup.findOne({ nick: req.params.nick });
+//     console.log(cup);
+//     if (!cup) {
+//       throw new Error("Нет такого!");
+//     }
+//     res.render('cup', {
+//       title: cup.title,
+//       picture: cup.avatar,
+//       desc: cup.desc
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 
 
 
 
-module.exports = router;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const Cup = require("../models/cup").Cup
 
+
+// router.get('/', function(req, res, next) {
+//     res.render('index', { title: 'Coffee' });
+//   });
+
+
+/* GET home page. */
+router.get('/', async (req, res, next) => {
+  try {
+    const menu = await Cup.find({}, { _id: 0, title: 1, nick: 1 });
+    res.render('index', {
+      title: 'Cup',
+      menu: menu
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = router;

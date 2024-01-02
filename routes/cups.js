a56da0129e39dda6118c209c2dfd9273f65d4322
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2024-01-02 19:57:50 +0300

    ВЫВОД ДАННЫХ НА ЭКРАНАХ

--- routes/cups.js
+++ routes/cups.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const Cup = require("../models/cup").Cup;
+// const Cup = require("../models/cup").Cup;
 var async = require("async")
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+// var checkAuth = require("./../middleware/checkAuth.js")
+
+
+router.get("/:nick", function(req, res, next) {
+  db.query(`SELECT * FROM cups WHERE cups.nick = '${req.params.nick}'`, (err,cups) => {
+  if(err) {
+    console.log(err);
+    if(err) return next(err)
+    } else {
+      if(cups.length == 0) return next(new Error("Такого напитка нет"))
+        var cup = cups[0];
+        res.render('cup', {
+          title: cup.title,
+          picture: cup.avatar,
+          desc: cup.about
+  })
+}
+})
+});
+
 
 // /* GET users listing. */
 // router.get('/', (req, res, next) => {
@@ -10,33 +30,34 @@ var checkAuth = require("./../middleware/checkAuth.js")
 // });
 
 
-/* Страница героев */
-  router.get('/:nick', checkAuth, async function(req, res, next) {
-    try {
-      const [cup, cups] = await Promise.all([Cup.findOne({ nick: req.params.nick }), Cup.find({}, { _id: 0, title: 1, nick: 1 })
-      ]);
+// /* Страница героев */
+//   router.get('/:nick', checkAuth, async function(req, res, next) {
+//     try {
+//       const [cup, cups] = await Promise.all([Cup.findOne({ nick: req.params.nick }), Cup.find({}, { _id: 0, title: 1, nick: 1 })
+//       ]);
     
-      if (!cup) {
-        throw new Error("Нет такого");
-      }
+//       if (!cup) {
+//         throw new Error("Нет такого");
+//       }
       
-      renderCup(res, cup.title, cup.avatar, cup.desc, cups);
-    } catch (err) {
-      next(err);
-    }
-  });
+//       renderCup(res, cup.title, cup.avatar, cup.desc, cups);
+//     } catch (err) {
+//       next(err);
+//     }
+//   });
   
-  function renderCup(res, title, picture, desc, cups) {
-    console.log(cups);
+//   function renderCup(res, title, picture, desc, cups) {
+//     console.log(cups);
   
-    res.render('cup', {
-      title: title,
-      picture: picture,
-      desc: desc
-    });
-  }
+//     res.render('cup', {
+//       title: title,
+//       picture: picture,
+//       desc: desc
+//     });
+//   }
   
   
+
   
   module.exports = router;
   

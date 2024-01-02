bfd94185a1e9b698380fe750546388be4fdddba8
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2024-01-02 20:02:50 +0300

    ЗАПОЛНЕНИЕ МЕНЮ ИЗ БАЗЫ

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,13 +1,12 @@
 const Cup = require("./../models/cup").Cup;
 
 module.exports = async function(req, res, next) {
-    try {
-      res.locals.nav = [];
-      const result = await Cup.find({}, { _id: 0, title: 1, nick: 1 });
-      res.locals.nav = result;
-      next();
-
-    } catch (err) {
-      throw err;
-    }
+  try {
+    res.locals.nav = [];
+    const result = await Cup.find(null, { _id: 0, title: 1, nick: 1 });
+    res.locals.nav = result;
+    next();
+  } catch (err) {
+    throw err;
+  }
 };

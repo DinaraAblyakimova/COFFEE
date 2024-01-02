134e622adbdafef683675d38cd3467cdb7988eef
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2024-01-02 19:20:50 +0300

    ЗАКРЫТИЕ СТРАНИЦ САЙТА ДЛЯ НЕЗАЛОГИНЕННОГО ПОЛЬЗОВАТЕЛЯ

--- routes/cups.js
+++ routes/cups.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Cup = require("../models/cup").Cup;
 var async = require("async")
+var checkAuth = require("./../middleware/checkAuth.js")
 
 // /* GET users listing. */
 // router.get('/', (req, res, next) => {
@@ -10,7 +11,7 @@ var async = require("async")
 
 
 /* Страница героев */
-  router.get('/:nick', async function(req, res, next) {
+  router.get('/:nick', checkAuth, async function(req, res, next) {
     try {
       const [cup, cups] = await Promise.all([Cup.findOne({ nick: req.params.nick }), Cup.find({}, { _id: 0, title: 1, nick: 1 })
       ]);
